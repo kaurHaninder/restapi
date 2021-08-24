c4200279c680180ed57c507c2d9dd1a4684b6e01
Haninder <haninderkaur0410@gmail.com>
2021-08-24 09:52:28 +0530

    first commit

--- myproject/api_basic/views.py
+++ myproject/api_basic/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
-
+from rest_framework import generics
 # Create your views here.
 
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article
+    serializer_class = ArticleSerializer
 
-def article_list(request):
-    if request.method == "GET":
-        article = Article.objects.all()
-        serializer = ArticleSerializer(article , many=True)
-        return JsonResponse(serializer ,safe=False)
-    elif request.method == "POST":
-        data = JSONParser.parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+
+
+
+# def article_list(request):
+#     if request.method == "GET":
+#         article = Article.objects.all()
+#         serializer = ArticleSerializer(article , many=True)
+#         return JsonResponse(serializer ,safe=False)
+#     elif request.method == "POST":
+#         data = JSONParser.parse(request)
+#         serializer = ArticleSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from api_basic.views import ArticleList , ArticleDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api_basic.urls'))
+    path('api/book',ArticleList.as_view()),
+    path('api/book/<int:pk>',ArticleDetail.as_view()),
 ]
